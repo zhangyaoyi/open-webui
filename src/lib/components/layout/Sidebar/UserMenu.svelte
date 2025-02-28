f79419d909da61f7f8e94dc3843d1f8cf9b706a4
Leo Cheung <zhangyaoyi2011@hotmail.com>
2025-02-28 11:06:16 +0800

    dev

--- src/lib/components/layout/Sidebar/UserMenu.svelte
+++ src/lib/components/layout/Sidebar/UserMenu.svelte
@@ -184,7 +184,7 @@
 				<div class=" self-center truncate">{$i18n.t('Sign Out')}</div>
 			</button>
 
-			{#if $activeUserIds?.length > 0}
+			<!-- {#if $activeUserIds?.length > 0}
 				<hr class=" border-gray-100 dark:border-gray-850 my-1 p-0" />
 
 				<Tooltip
@@ -212,7 +212,7 @@
 						</div>
 					</div>
 				</Tooltip>
-			{/if}
+			{/if} -->
 
 			<!-- <DropdownMenu.Item class="flex items-center px-3 py-2 text-sm ">
 				<div class="flex items-center">Profile</div>
